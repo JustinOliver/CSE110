dbb4e5e5ebf6759ccc0e9b438362544927e6ff87
Justin Oliver <JustinOliver77@aol.com>
2019-04-20 21:39:06 -0700

    Add files via upload

---
+++ bonus.py
@@ -0,0 +1,31 @@
+email = input("What is your email address?:")
+lowerEmail = email.lower()
+splitEmail = lowerEmail.split('@')
+astrixEmail = (splitEmail[0][0]) + ("******") + (splitEmail[0][-1])
+joinedEmail = astrixEmail + "@" + splitEmail[1]
+print(joinedEmail)
+
+
+number = input("What is your phone number?")
+removeChars = ['+','(', ')', ' ', '-']
+for char in removeChars:
+	number = number.replace(char, '')
+localNumber = number[-10:]
+localNumberList = list(localNumber)
+for i in range(len(localNumberList[:-4])):
+	localNumberList[i] = '*'
+localNumberList.insert(-7,"-")
+localNumberList.insert(-4,"-")
+newLocalNumber = "".join(localNumberList)
+
+if len(number) == 13 :
+	newLocalNumber = "+***-" + newLocalNumber
+elif len(number) == 12:
+	newLocalNumber ="+**-" + newLocalNumber
+elif len(number) == 11:
+	newLocalNumber = "+*-" + newLocalNumber
+else:
+	pass
+
+print(newLocalNumber)
+
